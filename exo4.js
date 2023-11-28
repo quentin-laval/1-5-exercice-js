3f40537ff246bb76c6c1a46d885484ec595ea3cd
quentin laval <laval.quentin80@gmail.com>
2023-11-28 14:17:02 +0100

    js

---
+++ exo4.js
@@ -0,0 +1,12 @@
+// alert("toto");
+
+var MavariableString = "toto";
+var MavariableNumerique = 100;
+var Mavaraibletableau = ["Apple", "Banana"];
+var MavariableObjet = window;
+// var MavariableNumerique = 100;
+// var Mavaraibletableau = ["Apple", "Banana"];
+// var MavariableObjet = window;
+// alert(MavariableString + MavariableNumerique + Mavaraibletableau[0] + MavariableObjet.location.pathname);
+
+var MavariableNumerique = prompt("entrez un nombre", 100);
---
+++ exo5.js
@@ -0,0 +1,23 @@
+// alert("toto");
+// alert("HelloWord");
+
+var MavariableString = "toto";
+// var MavariableNumerique = 100;
+// var Mavaraibletableau = ["Apple", "Banana"];
+// var MavariableObjet = window;
+var MavariableObjet = window;
+// alert(MavariableString + MavariableNumerique + Mavaraibletableau[0] + MavariableObjet.location.pathname);
+
+var MavariableNumerique = prompt("entrez un nombre", 100);
+// var MavariableNumerique = prompt("entrez un nombre", 100);
+
+if (MavariableString == "toto" && MavariableNumerique != 200 || MavariableNumerique <= 100)
+if (MavariableObjet.innerWidth < 300)
+{
+    alert("je suis le cas 1");
+}
+else {
+    alert("je suis le cas 2");
+    alert("alerte !");
+
+}
